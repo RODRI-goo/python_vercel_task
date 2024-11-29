11e50ac67db8711f4908106b87c7ada4dada241e
RODRI-goo <rodrigonoguera1103@gmail.com>
2024-11-29 20:40:11 -0300

    segundo commit

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'python_vercel_task.wsgi.application'
+WSGI_APPLICATION = 'python_vercel_task.wsgi.app'
 
 
 # Database
